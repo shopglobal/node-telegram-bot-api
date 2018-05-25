46f3405f02d8cdcf6f9d9d74bf038e9bb4071854
Interchained <president@worldvaporexpo.com>
2018-05-24 20:59:13 -0400

    Update game.js

--- examples/game/game.js
+++ examples/game/game.js
@@ -3,11 +3,11 @@
  */
 /* eslint-disable no-console */
 
-const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
-const gameName = process.env.TELEGRAM_GAMENAME || 'YOUR_TELEGRAM_GAMENAME';
+const TOKEN = process.env.TELEGRAM_TOKEN || '522621342:AAHXORwiGtHDi5CeeTXMQJ59hn5F1BAxVzI';
+const gameName = process.env.TELEGRAM_GAMENAME || 'mining';
 // Specify '0' to use ngrok i.e. localhost tunneling
-let url = process.env.URL || 'https://<PUBLIC-URL>';
-const port = process.env.PORT || 8080;
+let url = process.env.URL || 'https://tel.electronero.org';
+const port = process.env.PORT || 8084;
 
 const TelegramBot = require('../..');
 const express = require('express');
@@ -16,9 +16,31 @@ const path = require('path');
 const bot = new TelegramBot(TOKEN, { polling: true });
 const app = express();
 
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Calculating awesome response, beep bop boop');
+
+
 // Basic configurations
 app.set('view engine', 'ejs');
-
+});
 // Tunnel to localhost.
 // This is just for demo purposes.
 // In your application, you will be using a static URL, probably that
